fd249eea0233b2d6a5cb828cb264004e6dee7b51
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-19 20:32:44 -0700

    Update html, add patterns object, validation function and event listener to javascript file.

---
+++ validation.js
@@ -0,0 +1,28 @@
+//first way to declare a regex expression is to store it in a variable
+// let reg = /[a-z]/i;
+
+//second way is to declare new regex obj and pass two arguments, the regular expression and the flags you want, in string form
+// let reg2 = new RegExp(/[a-z]/, 'i');
+
+const userInputs = document.querySelectorAll("input"); //collection of elements
+
+const regPatterns = {
+  telephone: /^\d{10}$/,
+
+}
+
+// validation function
+function validate(field, regex){
+  if(regex.test(field.value)){  //check that the regex pattern is the same as the value of the field
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
+}
+
+userInputs.forEach((input) => {
+  input.addEventListener("keyup", (event) => {
+    // console.log(event.target.attributes.name.value);
+    validate(event.target, regPatterns[event.target.attributes.name.value]);
+  });
+});
