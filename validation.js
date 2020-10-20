da73818897d37c8470be7361f13325e024ef495a
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-19 21:07:07 -0700

    Add email and slug to regPatterns object in js file.

--- validation.js
+++ validation.js
@@ -4,12 +4,22 @@
 //second way is to declare new regex obj and pass two arguments, the regular expression and the flags you want, in string form
 // let reg2 = new RegExp(/[a-z]/, 'i');
 
+//RegEx for email address:
+//(yourname)@(domain).(extension).(againextension), for example
+// theboss@thenetninja.co.uk
+// 1. any letters, numbers, dots and/or hyphens
+// 2. any letters, numbers and/or hyphens
+// 3. any letters
+// 4. a dot (.) then any letters
+
 const userInputs = document.querySelectorAll("input"); //collection of elements
 
 const regPatterns = {
   username: /^[a-z\d]{5,12}$/i,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
   telephone: /^\d{10}$/,
-  password: /^[@-\w]{8,20}$/
+  password: /^[@-\w]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/
 }
 
 // validation function
